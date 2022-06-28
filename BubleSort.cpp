27efb9b7a395e550ccbc8d6ce6b069b881d4ae4c
Dead Ghoul <90885787+Doath1337@users.noreply.github.com>
2022-06-28 21:40:58 +0700

    Bubble

---
+++ BubleSort.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int NumOfElements;
+	std::cin >> NumOfElements;
+	std::vector<int> array(NumOfElements);
+	for (int i = 0; i < array.size(); ++i) {
+		int  num;
+		std::cin >> num;
+		array[i] = num;
+	}
+
+	for (int j = 0; j < array.size(); ++j)
+	{
+		
+		for (int n = 0; n < array.size()-1;++n) {
+			if (array[n] > array[n + 1]) {
+				std::swap(array[n], array[n + 1]);
+
+			}
+
+		}
+	}
+	for (int i = 0; i < array.size(); ++i) {
+		std::cout << array[i] << std::endl;
+	}
+}
