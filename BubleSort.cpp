86a73d216da794b40bd660a3b6e04d840355428e
Dead Ghoul <90885787+Doath1337@users.noreply.github.com>
2022-06-29 13:53:02 +0700

    update

--- BubleSort.cpp
+++ BubleSort.cpp
@@ -6,8 +6,10 @@
 int main()
 {
 	int NumOfElements;
+	std::cout << "Enter number of variables: ";
 	std::cin >> NumOfElements;
 	std::vector<int> array(NumOfElements);
+	std::cout << "Enter " << NumOfElements << " numbers separated by a SPACE: " << std::endl;
 	for (int i = 0; i < array.size(); ++i) {
 		int  num;
 		std::cin >> num;
@@ -16,8 +18,8 @@ int main()
 
 	for (int j = 0; j < array.size(); ++j)
 	{
-		
-		for (int n = 0; n < array.size()-1;++n) {
+
+		for (int n = 0; n < array.size() - 1; ++n) {
 			if (array[n] > array[n + 1]) {
 				std::swap(array[n], array[n + 1]);
 
@@ -25,6 +27,9 @@ int main()
 
 		}
 	}
+
+	std::cout << std::endl;
+	std::cout << "ARRAY HAS BEEN SORTED SUCCESSFULLY" << std::endl;
 	for (int i = 0; i < array.size(); ++i) {
 		std::cout << array[i] << std::endl;
 	}
